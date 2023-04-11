14dd4b614cb2363976677aff8d57d82df1602ded
Abrarkotb <127283773+Abrarkotb@users.noreply.github.com>
2023-04-12 00:54:56 +0200

    Add files via upload

---
+++ LAB2/lab2.4/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{     int s;
+     int arr[s] ;
+     printf("please enter the size of arry %d \n");
+     scanf("%d",&s);
+
+    for(int i=0 ; i<s ;i++){
+        printf("please enter number %d \n",i+1);
+        scanf("%d",&arr[i]);
+    }
+   for(int i=0 ; i<s ;i--){
+        printf("please enter number %d \n",arr[i]);
+    }
+}
+
+
+
+
+   char ch[10];
+
+
+  /* printf("please enter s srting\n");
+   gets(ch);
+
+   strrev(ch);
+
+   printf("your string is: %s \n", ch);*/
+
+l}
